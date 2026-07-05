d0d59a208aad1bd7fb4655a4e5fb736be25a56d5
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2026-07-05 17:32:53 +0530

    Time: 0 ms (100%), Space: 53.2 MB (77.28%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,10 +3,10 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    let n = s.length - 1;
+    let pointer = s.length - 1;
     let count = 0;
 
-    for(let i = n; i >=0; i--){
+    for(let i = pointer; i >= 0; i--){
         if(s[i] !== " "){
             count++;
         }else if(count > 0){
