ec233aa8b00d00c7eed154ceef2a4ae9b4628809
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2026-07-06 21:22:24 +0530

    Time: 6 ms (69.91%), Space: 55.6 MB (31.8%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    let x = {};
+    let y = {};
+
+    for(let i = 0; i < s.length; i++){
+        if(!x[s[i]] && !y[t[i]]){
+            x[s[i]] = t[i];
+            y[t[i]] = s[i];
+        }else if(x[s[i]] !== t[i] || y[t[i]] !== s[i]){
+            return false;
+        }
+    }   
+    return true;
+};
