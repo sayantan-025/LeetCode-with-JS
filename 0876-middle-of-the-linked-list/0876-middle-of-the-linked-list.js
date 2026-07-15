7dbd1647c4edb7b6d699f9d49bf0855c71620849
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2026-07-15 19:08:42 +0530

    Time: 0 ms (100%), Space: 54 MB (18.07%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -10,13 +10,11 @@
  * @return {ListNode}
  */
 var middleNode = function(head) {
-    let slow = head;
-    let fast = head;
+    let slow = fast = head;
 
     while(fast && fast.next){
         slow = slow.next;
         fast = fast.next.next;
     }
-
     return slow;
 };
