a0adc1661d43040f89e1c75f13baec126bf16414
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2026-07-19 18:47:55 +0530

    Time: 0 ms (100%), Space: 58.6 MB (15.65%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let count = 0;
+
+    for(let i = 1; i < nums.length; i++){
+        if(nums[count] !== nums[i]){
+            ++count;
+            nums[count] = nums[i];
+        }
+    }
+    return count + 1;
+};
