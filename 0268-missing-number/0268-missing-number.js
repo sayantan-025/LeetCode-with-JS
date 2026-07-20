79b4176ba9adc14463bb97d6de6ce3f9765c72cb
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2026-07-20 21:04:06 +0530

    Time: 1 ms (68.82%), Space: 56.3 MB (60.75%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let n = nums.length;
+    let total_sum = n * (n + 1) / 2;
+    let sum = 0;
+
+    for(let i = 0; i < nums.length; i++){
+        sum += nums[i];
+    }
+
+    return total_sum - sum;
+};
