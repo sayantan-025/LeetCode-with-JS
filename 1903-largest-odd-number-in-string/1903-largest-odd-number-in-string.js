5243a860b9f43950271e6312d1a49794064e165a
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2026-07-21 15:06:25 +0530

    Time: 2 ms (37.86%), Space: 58.3 MB (64.37%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.js
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.js
@@ -3,12 +3,12 @@
  * @return {string}
  */
 var largestOddNumber = function(num) {
-    let n = num.length - 1;
+    let x = num.length - 1;
 
-    for(let i = n; i >= 0; i--){
+    for(let i = x; i >= 0; i--){
         if(num[i] % 2 === 1){
             return num.substring(0 , i+1);
         }
     }
-    return "";
+    return  "";
 };
