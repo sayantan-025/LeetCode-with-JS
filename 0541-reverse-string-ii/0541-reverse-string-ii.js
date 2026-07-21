3c1ec8bed748e5cc8e2caa210b53a5f1108c1231
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2026-07-21 14:52:04 +0530

    Time: 4 ms (67.45%), Space: 55.2 MB (92.38%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.js
+++ 0541-reverse-string-ii/0541-reverse-string-ii.js
@@ -6,17 +6,16 @@
 var reverseStr = function(s, k) {
     s = s.split("");
 
-    for(let x = 0 ; x < s.length; x += 2 * k){
-       let i = x;
-       let j = Math.min(x+k-1, s.length-1);
-
-       while(i < j){
-        let temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
-        i++;
-        j--;
-       }
+    for(let i = 0; i < s.length; i += 2 * k){
+        let x = i;
+        let z = Math.min(i+k-1, s.length - 1);
+        while(x < z){
+            let temp = s[x];
+            s[x] = s[z];
+            s[z] = temp;
+            x++;
+            z--;
+        }
     }
 
     return s.join("");
