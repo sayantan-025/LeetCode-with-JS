fad9e22819dc25df1304d7cc10212ad79ebc2e37
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2026-07-21 14:44:55 +0530

    Time: 0 ms (100%), Space: 53.7 MB (79.91%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
@@ -6,16 +6,17 @@ var balancedStringSplit = function(s) {
     let track = 0;
     let count = 0;
 
-    for(let i = 0 ; i < s.length; i++){
-        if(s[i] === "R"){
-           track++;
+    for(let i = 0; i < s.length; i++){
+        if(s[i] === "L"){
+            track++;
         }else{
             track--;
         }
 
         if(track === 0){
-            count++;
+           count++;
         }
     }
+
     return count;
 };
