387177ba1859b0efba7d9618e24da818845b3254
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2026-07-21 15:14:05 +0530

    Time: 8 ms (89.35%), Space: 54.8 MB (91.87%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -9,16 +9,16 @@ var isAnagram = function(s, t) {
     if(s.length !== t.length) return false;
 
     for(let i = 0; i < s.length; i++){
-        if(!map[s[i]]){
-            map[s[i]] = 1;
-        }else{
+        if(map[s[i]]){
             map[s[i]]++;
+        }else{
+            map[s[i]] = 1;
         }
     }
 
     for(let i = 0; i < t.length; i++){
         if(map[t[i]]){
-           map[t[i]]--;
+            map[t[i]]--;
         }else{
             return false;
         }
