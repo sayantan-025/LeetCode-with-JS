2383a5f29de5b5eaccfa2694dd64bf40d95dc15b
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2026-08-02 11:38:33 +0530

    Time: 1 ms (17%), Space: 54 MB (28.39%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.js
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.js
@@ -1,21 +1,21 @@
 
-var MyQueue = function () {
+var MyQueue = function() {
     this.s1 = [];
-    this.s2 = []; 
+    this.s2 = [];
 };
 
 /** 
  * @param {number} x
  * @return {void}
  */
-MyQueue.prototype.push = function (x) {
+MyQueue.prototype.push = function(x) {
     this.s1.push(x);
 };
 
 /**
  * @return {number}
  */
-MyQueue.prototype.pop = function () {
+MyQueue.prototype.pop = function() {
     if(this.s2.length === 0){
         while(this.s1.length){
             this.s2.push(this.s1.pop());
@@ -27,8 +27,8 @@ MyQueue.prototype.pop = function () {
 /**
  * @return {number}
  */
-MyQueue.prototype.peek = function () {
-   if(this.s2.length === 0){
+MyQueue.prototype.peek = function() {
+    if(this.s2.length === 0){
         while(this.s1.length){
             this.s2.push(this.s1.pop());
         }
@@ -39,8 +39,8 @@ MyQueue.prototype.peek = function () {
 /**
  * @return {boolean}
  */
-MyQueue.prototype.empty = function () {
-   return this.s1.length === 0 && this.s2.length === 0;
+MyQueue.prototype.empty = function() {
+    return this.s1.length === 0 && this.s2.length === 0;
 };
 
 /** 
