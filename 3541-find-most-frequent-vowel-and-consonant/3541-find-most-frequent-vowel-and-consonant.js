ad1752a692689ca996d7f470b7b6325b3532b8c4
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2026-08-04 19:57:28 +0530

    Time: 11 ms (27.47%), Space: 56.7 MB (73.44%) - LeetHub

--- 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.js
+++ 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.js
@@ -6,22 +6,22 @@ var maxFreqSum = function(s) {
     let map = {};
 
     for(let i = 0; i < s.length; i++){
-        if(map[s[i]]){
-            map[s[i]]++;
+        if(!map[s[i]]){
+           map[s[i]] = 1;
         }else{
-            map[s[i]] = 1;
+            map[s[i]]++;
         }
     }
 
-    let vowel = ["a", "e", "i", "o", "u"];
+    let vowel = ["a" , "e" , "i" , "o" , "u"];
     let max_vowel = 0;
     let max_cons = 0;
 
     for(let i = 0; i < s.length; i++){
         if(vowel.includes(s[i])){
-            max_vowel = Math.max(max_vowel, map[s[i]]);
+            max_vowel = Math.max(max_vowel,map[s[i]]);
         }else{
-            max_cons = Math.max(max_cons, map[s[i]]);
+            max_cons = Math.max(max_cons,map[s[i]]);
         }
     }
     return max_vowel + max_cons;
