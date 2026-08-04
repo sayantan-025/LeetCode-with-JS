09b611cd7fbad5bdfb59ee1d9a373e63f9150d97
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2026-08-04 19:43:58 +0530

    Time: 1 ms (83.97%), Space: 58.3 MB (51.32%) - LeetHub

--- 2942-find-words-containing-character/2942-find-words-containing-character.js
+++ 2942-find-words-containing-character/2942-find-words-containing-character.js
@@ -4,15 +4,15 @@
  * @return {number[]}
  */
 var findWordsContaining = function(words, x) {
-   let arr = [];
+    let ans = [];
 
     for(let i = 0; i < words.length; i++){
         for(let j = 0; j < words[i].length; j++){
             if(words[i][j] === x){
-               arr.push(i);
-               break;
+                ans.push(i);
+                break;
             }
         }
     }
-    return arr;
+    return ans;
 };
