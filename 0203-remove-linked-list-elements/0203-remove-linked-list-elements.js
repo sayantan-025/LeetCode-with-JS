fb3854a3fe21930fd1cd9a298c25c5fc640920ea
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2026-08-10 11:32:55 +0530

    Time: 1 ms (49.34%), Space: 62.1 MB (16.84%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.js
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.js
@@ -11,17 +11,17 @@
  * @return {ListNode}
  */
 var removeElements = function(head, val) {
-    let sentinal = new ListNode();
-    sentinal.next = head;
+  let sentinal = new ListNode();
+  sentinal.next = head;
+  
+  let prev = sentinal;
 
-    let prev = sentinal;
-
-    while(prev && prev.next){
-        if(prev.next.val === val){
-            prev.next = prev.next.next;
-        }else{
-            prev = prev.next;
-        }
+  while(prev && prev.next){
+    if(prev.next.val === val){
+        prev.next = prev.next.next;
+    }else{
+        prev = prev.next;
     }
-    return sentinal.next;
+  }  
+  return sentinal.next;
 };
