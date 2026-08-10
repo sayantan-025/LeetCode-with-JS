37d1fb4985ea3bc6703b07ef1a425c99baf630b7
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2026-08-10 09:43:05 +0530

    Time: 66 ms (7.52%), Space: 59.2 MB (48.28%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,17 +11,17 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    if(head === null) return false;
-    
-    let slow  = head;
+    if(head  === null) return false;
+
+    let slow = head;
     let fast = head.next;
 
     while(slow !== fast){
-        if(!fast || !fast.next){
-            return false;
-        }
-        slow = slow.next;
-        fast = fast.next.next;
+       if(!fast || !fast.next){
+        return false;
+       }
+       slow = slow.next;
+       fast = fast.next.next;
     }
     return true;
 };
