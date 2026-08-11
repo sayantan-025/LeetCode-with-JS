798c588c26267f59e0ef617ab3266eb86c01ae4a
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2026-08-11 22:36:38 +0530

    Time: 0 ms (100%), Space: 53 MB (87.02%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.js
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.js
@@ -28,21 +28,19 @@ MyQueue.prototype.pop = function() {
  * @return {number}
  */
 MyQueue.prototype.peek = function() {
-    if(this.s2.length === 0){
+     if(this.s2.length === 0){
         while(this.s1.length){
             this.s2.push(this.s1.pop());
         }
     }
-    let ans =  this.s2.pop();
-    this.s2.push(ans);
-    return ans;
+    return this.s2[this.s2.length - 1];
 };
 
 /**
  * @return {boolean}
  */
 MyQueue.prototype.empty = function() {
-    return this.s1.length === 0 && this.s2.length === 0;
+   return this.s1.length === 0 && this.s2.length === 0;
 };
 
 /** 
