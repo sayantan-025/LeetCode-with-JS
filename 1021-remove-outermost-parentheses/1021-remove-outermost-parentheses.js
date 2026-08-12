895162ebb2d6ea1ddfc7b980025b4fba1936d960
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2026-08-12 21:11:46 +0530

    Time: 4 ms (57.99%), Space: 57.1 MB (37.08%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.js
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.js
@@ -3,8 +3,8 @@
  * @return {string}
  */
 var removeOuterParentheses = function(s) {
-    let ans = "";  
-    let count = 0; 
+    let ans = "";
+    let count = 0;
 
     for(let i = 0; i < s.length; i++){
         if(s[i] === ")") count--;
