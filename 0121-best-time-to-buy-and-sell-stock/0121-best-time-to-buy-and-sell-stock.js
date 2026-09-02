89fbe3620510ef0907b585ba6ebd5665fb059929
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2026-09-02 19:30:27 +0530

    Time: 2 ms (80.22%), Space: 64.5 MB (85.91%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,15 +3,15 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-   let min_val = prices[0];
-   let max_profit = 0;
+    let min_price = prices[0];
+    let max_profit = 0;
 
-   for(let i = 1; i < prices.length; i++){
-      if(prices[i] < min_val){
-        min_val = prices[i];
-      }else{
-        max_profit = Math.max(max_profit, prices[i] - min_val);
-      }
-   }
-   return max_profit;
+    for(let i = 1; i < prices.length; i++){
+        if(prices[i] < min_price){
+            min_price = prices[i];
+        }else{
+            max_profit = Math.max(max_profit, prices[i] - min_price);
+        }
+    }
+    return max_profit;
 };
