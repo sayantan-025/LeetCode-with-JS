8d5a6798b3fab22cafc4e8698d6dc01d05f3255d
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2026-09-03 19:22:45 +0530

    Time: 0 ms (100%), Space: 60.9 MB (19.25%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,14 +3,14 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let tracker = 0;
+    let n = 0;
 
     for(let i = 0; i < nums.length; i++){
         if(nums[i] !== 0){
-            let temp = nums[tracker];
-            nums[tracker] = nums[i];
+            let temp = nums[n];
+            nums[n] = nums[i];
             nums[i] = temp;
-            tracker++;
+            n++;
         }
     }
     return nums;
