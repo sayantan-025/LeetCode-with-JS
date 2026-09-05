5856d7e4b671f87a23bd6949ed3f29a5db25ceb8
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2026-09-05 10:43:07 +0530

    Time: 1 ms (92.2%), Space: 58.8 MB (71.75%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.js
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -3,16 +3,17 @@
  * @return {number}
  */
 var findMaxConsecutiveOnes = function(nums) {
+    let ans = 0;
     let count = 0;
-    let max_count = 0;
 
     for(let i = 0; i < nums.length; i++){
         if(nums[i] === 1){
             count++;
-            max_count = Math.max(count, max_count);
+            ans = Math.max(ans,count);
+            
         }else{
             count = 0;
         }
     }
-    return max_count;
+    return ans;
 };
