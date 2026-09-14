377e6a86bf54ce17fae82cc7f322d601b6bffcfb
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2026-09-14 15:10:52 +0530

    Time: 1 ms (88.98%), Space: 54.8 MB (53.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -7,12 +7,12 @@ var twoSum = function(nums, target) {
     let map = new Map();
 
     for(let i = 0; i < nums.length; i++){
-        let complement = target - nums[i];
+        let x = target - nums[i];
 
-        if(map.has(complement)){
-            return [map.get(complement),i];
+        if(map.has(x)){
+            return [map.get(x), i];
         }else{
-            map.set(nums[i],i);
+            map.set(nums[i], i);
         }
     }
 };
