772d9e415aeda7c3f60e046d6412e61825d6a638
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2026-09-14 14:37:19 +0530

    Time: 30 ms (32.22%), Space: 79.2 MB (62.28%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,14 +3,5 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let set = new Set();
-
-    for(let i = 0; i < nums.length; i++){
-        if(set.has(nums[i])){
-            return true;
-        }else{
-            set.add(nums[i]);
-        }
-    }
-    return false;
+    return new Set(nums).size < nums.length;
 };
